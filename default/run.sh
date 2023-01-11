47419f88abbd6ea855ea635d4851b1ab8815ac6e
JunnyJun <chbe5082@naver.com>
2023-01-11 22:37:09 +0900

    elk

---
+++ default/run.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "SET TIME ZONE"
+sudo timedatectl set-timezone Asia/Seoul
+sudo timedatectl set-local-rtc no
+sudo timedatectl set-ntp yes
+
+sudo apt install -y tzdata
+ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+
+echo "SET LANG"
+sudo apt-get install locales
+sudo dpkg-reconfigure locales
+sudo locale-gen ko_KR.UTF-8
+sudo update-locale LC_ALL=ko_KR.UTF-8 LANG=ko_KR.UTF-8
+export LANG=ko_KR.UTF-8
+export LC_ALL=ko_KR.UTF-8
