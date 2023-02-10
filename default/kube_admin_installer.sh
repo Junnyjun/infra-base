67202e4202bb455342e2ed60091db454c424593d
Junny <72375574+Junnyjun@users.noreply.github.com>
2023-02-10 15:53:50 +0900

    Update kube_admin_installer.sh

--- default/kube_admin_installer.sh
+++ default/kube_admin_installer.sh
@@ -1,3 +1,25 @@
+echo "[INSTALL BASE PACKAGE]"
+sudo apt-get update
+ 
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+echo "[INSTALL docker archive]"
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "[INSTALL signing keyring]"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo "[INSTALL docker engine]"
+sudo apt-get update -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 echo "[swap off ]"
 sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
 
