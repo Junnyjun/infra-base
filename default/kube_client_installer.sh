53c73b1b39e59954c2f08465e47bc8419b415cb8
Junny <72375574+Junnyjun@users.noreply.github.com>
2023-02-10 16:03:00 +0900

    Update kube_client_installer.sh

--- default/kube_client_installer.sh
+++ default/kube_client_installer.sh
@@ -8,17 +8,17 @@ sudo apt-get install \
     lsb-release
 
 echo "[INSTALL docker archive]"
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo "[INSTALL signing keyring]"
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 
 echo "[INSTALL docker engine]"
 sudo apt-get update -y 
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "[swap off ]"
 sudo swapoff -a && sudo sed -i '/swap/s/^/#/' /etc/fstab
